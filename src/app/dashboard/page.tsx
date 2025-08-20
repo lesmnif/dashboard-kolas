6f5720edf36235bf64f59a454d10d52f561aa1ab
lesmnif <bogdan1850@gmail.com>
2025-08-20 19:06:25 +0200

    demo mode button change

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -640,7 +640,7 @@ export default function Dashboard() {
                       d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
                     />
                   </svg>
-                  Demo Mode
+                  Go to Demo Mode
                 </Link>
                 <div className="flex items-center space-x-2 px-3 py-1 bg-gray-100 rounded-md">
                   <svg
